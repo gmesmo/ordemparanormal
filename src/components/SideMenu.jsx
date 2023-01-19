390ef7310549eebb2db70df46ef6fd8b7934acfc
JustG <guimachado94@gmail.com>
2023-01-19 08:55:27 -0300

    1901

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './SideMenu.css'
 
 const SideMenu = () => {
+    
+    const [elementos, setElementos] = useState([
+        {id: 1, nome: "Conhecimento",active: false  },
+        {id: 2, nome: "Sangue"      ,active: false  },
+        {id: 3, nome: "Morte"       ,active: false  },
+        {id: 4, nome: "Energia"     ,active: false  },
+        {id: 5, nome: "Medo"        ,active: false  },
+    ]);
+
     return ( 
         <>
             <div className="SideMenu">
                 <div className="Side-Content">
-                    TESTE
+                    {elementos.map((elemento =>
+                        elemento.nome
+                    ))}
                 </div>
             </div>
         </>
